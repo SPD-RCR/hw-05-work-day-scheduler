a8e7db4661b90476486bcf554641cbe30b34cda8
SpeedRacer <robertgeorge2@gmail.com>
2021-05-01 23:36:25 -0400

    Updated currentDay to Jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
-var currentDay = document.getElementById("currentDay");
-currentDay = moment().format('LLLL');
+var currentDay = moment().format("LLLL");
+$("#currentDay").append(currentDay);
+
+// var currentDay = document.getElementById("currentDay");
+// currentDay = moment().format('LLLL');
 console.log("currentDay:", currentDay);
